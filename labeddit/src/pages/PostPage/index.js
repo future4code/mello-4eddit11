c707a5b29c54a6afaa800b29c23aae635c30e1ac
edupezz <edupezz@gmail.com>
2020-07-07 11:54:18 -0300

    Incremento, estrutura componente Post pronto

--- labeddit/src/pages/PostPage/index.js
+++ labeddit/src/pages/PostPage/index.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import { Post } from '../../services/api';
+import { PostPageContainer } from './styles';
 
 import { 
   Form,
   Input,
   Col,
-  Row
+  Row,
+  Button
 }
 from 'antd';
 
@@ -16,22 +18,87 @@ function PostPage() {
 
   const [text, setText] = useState();
   const [title, setTitle] = useState();
+  const [resp, setResp] = useState();
+  const { TextArea } = Input;
+
+  const token = window.localStorage.getItem('token');
+
+  let key = {
+    headers: {
+        auth: token
+    }
+}
 
   const body = {
     text: text,
     title: title
   }
 
+  const Postar = async() => {
+
+    await Post('/posts', key, body)
+    .then(response => {
+      setResp(response.data.success)
+      console.log(response)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+
+  console.log(resp)
+  
   return (
-    <>
-    <Row>
-      <Col span={24}>Teste</Col>
-    </Row>
-    <Row>
-      <Col span={24}>Teste2</Col>
-    </Row>
-      
-    </>
+
+    <PostPageContainer>
+    <Form onSubmitCapture={Postar}>
+      <Row gutter={[16, 16]}>
+        <Col span={8}>
+
+          <Form.Item
+          label="Título: "
+          name="titulo"
+          rules={[
+            {
+              required: true,
+              message: 'Insira o título da publicação',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        </Col>
+      </Row>
+
+      <Row gutter={[16, 16]}>
+        <Col span={8}>
+
+          <Form.Item
+            label="Mensagem: "
+            name="mensagem"
+            rules={[
+              {
+                required: true,
+                message: 'Insira a mensagem da publicação',
+              },
+            ]}
+          >
+            <TextArea autoSize={{ minRows: 2, maxRows: 7 }}/>
+          </Form.Item>
+
+        </Col>
+      </Row>
+
+      <Row>
+      <Col span={8}>
+          <Button type="primary" htmlType="submit" >
+            Comentar
+          </Button>
+        </Col>
+      </Row>
+    </Form>
+    </PostPageContainer>
   );
 }
 
---
+++ labeddit/src/pages/PostPage/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const PostPageContainer = styled.div `
+
+    width: 99%;
+`
