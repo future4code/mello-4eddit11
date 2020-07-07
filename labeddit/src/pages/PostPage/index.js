785b4d6fdd33e7d504243a8816a6fb4424384e6c
edupezz <edupezz@gmail.com>
2020-07-06 23:45:04 -0300

    Inicio pagina post, atualizando commits

--- labeddit/src/pages/PostPage/index.js
+++ labeddit/src/pages/PostPage/index.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { Post } from '../../services/api';
+
+import { 
+  Form,
+  Input,
+  Col,
+  Row
+}
+from 'antd';
+
+import 'antd/dist/antd.css';
+
 
 function PostPage() {
+
+  const [text, setText] = useState();
+  const [title, setTitle] = useState();
+
+  const body = {
+    text: text,
+    title: title
+  }
+
   return (
     <>
-      <p>Post Page</p>
+    <Row>
+      <Col span={24}>Teste</Col>
+    </Row>
+    <Row>
+      <Col span={24}>Teste2</Col>
+    </Row>
+      
     </>
   );
 }
