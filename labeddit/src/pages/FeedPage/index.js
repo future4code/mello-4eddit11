39ee9c6e1a462ff33bf6b0ff3558aa33350652db
edupezz <edupezz@gmail.com>
2020-07-07 15:17:06 -0300

    Incremento, correcao de BUG API, estrutura FeedPage pronta.

--- labeddit/src/pages/FeedPage/index.js
+++ labeddit/src/pages/FeedPage/index.js
@@ -1,6 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { Get } from '../../services/api';
 
 function FeedPage() {
+
+  const token = window.localStorage.getItem('token');
+  const [posts, setPosts] = useState([]);
+
+  const key = {
+    headers: {
+      Authorization: token
+    }
+  }
+
+  useEffect(() =>{
+    getPosts()
+  }, [setPosts])
+
+  const getPosts = async() => {
+
+    await Get('/posts', key)
+    .then(response =>{
+      setPosts(response.data.posts)
+      console.log(response)
+    })
+    .catch(error =>{
+      console.log(error)
+    })
+
+  }
+
+  console.log(posts)
+  console.log(key)
+
   return (
     <>
       <p>Fedd Page</p>
