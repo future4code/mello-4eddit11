d307584d84d917896601b5c08ad5dfc2227d6117
yuzokamoto <yuzokamoto@gmail.com>
2020-07-07 18:27:38 -0300

    LoginPage pronta

--- labeddit/src/routes.js
+++ labeddit/src/routes.js
@@ -1,44 +1,45 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
+import React, { useContext } from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 // pages and components
-import LoginPage from "./pages/LoginPage";
-import FeedPage from "./pages/FeedPage";
-import PostPage from "./pages/PostPage";
-import RegisterPage from "./pages/RegisterPage";
-import NotFoundPage from "./pages/NotFoundPage";
+import LoginPage from './pages/LoginPage';
+import FeedPage from './pages/FeedPage';
+import PostPage from './pages/PostPage';
+import RegisterPage from './pages/RegisterPage';
+import NotFoundPage from './pages/NotFoundPage';
 import CreatePost from './components/CreatePost/index';
-
+import SessionContext from './contexts/SessionContext';
 
 const Routes = () => {
-  
+  const { session } = useContext(SessionContext);
+
   return (
     <Switch>
-
-      <Route exact path="/">
-        <LoginPage />
+      <Route exact path='/'>
+        {/* Checa se o usuário já está logado */}
+        {/* Se estiver, a página de Login não é retornada */}
+        {session.authenticated ? <FeedPage /> : <LoginPage />}
       </Route>
 
-      <Route exact path="/register">
+      <Route exact path='/register'>
         <RegisterPage />
       </Route>
 
-      <Route exact path="/feed">
+      <Route exact path='/feed'>
         <FeedPage />
       </Route>
 
-      <Route exact path="/post/:id">
+      <Route exact path='/post/:id'>
         <PostPage />
       </Route>
 
-      <Route exact path="/createpost">
+      <Route exact path='/createpost'>
         <CreatePost />
       </Route>
 
-      <Route path="*">
+      <Route path='*'>
         <NotFoundPage />
       </Route>
-
     </Switch>
   );
 };
