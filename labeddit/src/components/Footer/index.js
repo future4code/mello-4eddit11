f350e8184b71c571a057448e361d8df884e67080
Jonyelson Lopes <jonyelson20@gmail.com>
2020-07-09 16:56:22 -0300

    modificações no footer

--- labeddit/src/components/Footer/index.js
+++ labeddit/src/components/Footer/index.js
@@ -4,47 +4,52 @@ import {
   FooterSocialNetwork,
   FooterContainer,
   FooterMenu,
-  FooterLogo
-}
-from './styles';
+  FooterLogo,
+} from "./styles";
 
-import { BulbTwoTone  } from '@ant-design/icons';
+import { BulbTwoTone } from "@ant-design/icons";
 
 function Footer() {
-
-  const user = window.localStorage.getItem('user');
+  const user = window.localStorage.getItem("user");
   const username = user.username;
 
-  console.log(user)
+  console.log(user);
 
   return (
-
     <FooterContainer>
       <FooterMenu>
         <ul>
-          <li><a href="#">Quem Somos</a></li>
-          <li><a href="#">Privacy Policy</a></li>
-          <li><a href="#">Trabalhe Conosco</a></li>
-          <li><a href="#">Assessoria de Imprensa</a></li>
-          <li><a href="#">Entre em Contato Conosco</a></li>
+          <li>
+            <a href="#">Quem Somos</a>
+          </li>
+          <li>
+            <a href="#">Privacy Policy</a>
+          </li>
+          <li>
+            <a href="#">Trabalhe Conosco</a>
+          </li>
+          <li>
+            <a href="#">Assessoria de Imprensa</a>
+          </li>
+          <li>
+            <a href="#">Entre em Contato Conosco</a>
+          </li>
         </ul>
       </FooterMenu>
 
       <FooterLogo>
-          <BulbTwoTone twoToneColor="#FA4400"
-            style={{
-            fontSize: '4.5em',
-            }}/>
+        <BulbTwoTone
+          twoToneColor="#FA4400"
+          style={{
+            fontSize: "4.5em",
+          }}
+        />
       </FooterLogo>
 
       <FooterSocialNetwork />
       <hr />
-
     </FooterContainer>
-
   );
-
-
 }
 
 export default Footer;
